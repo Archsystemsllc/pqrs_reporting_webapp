f26908184a64b4e80b81c8c6d95d265eedbd58e5
MurugarajKandaswam <MurugarajKandaswam@10.1.10.250>
2017-06-26 17:17:54 -0400

    Screen 2 (List of Data Analysis) Implementation - Back End Service

---
+++ src/main/java/com/archsystemsinc/pqrs/repository/DataAnalaysisRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.archsystemsinc.pqrs.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.archsystemsinc.pqrs.model.DataAnalysis;
+
+/**
+ * @author MurugarajKandaswam
+ *
+ */
+public interface DataAnalaysisRepository extends JpaRepository<DataAnalysis, Long> {
+
+}

2a491df5c66ca805cb0bdc3c920607fdb7d5ae50
MurugarajKandaswam <MurugarajKandaswam@10.1.10.250>
2017-06-28 11:31:02 -0400

    Integration of Sub Data Analysis with Chart/Map

--- src/main/java/com/archsystemsinc/pqrs/model/SubDataAnalysis.java
+++ src/main/java/com/archsystemsinc/pqrs/model/SubDataAnalysis.java
@@ -2,6 +2,7 @@ package com.archsystemsinc.pqrs.model;
 
 import java.io.Serializable;
 import javax.persistence.*;
+import java.util.List;
 
 
 /**
@@ -15,6 +16,8 @@ public class SubDataAnalysis implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(unique=true, nullable=false)
 	private int id;
 
 	@Column(name="sub_data_analysis_description")
@@ -22,6 +25,10 @@ public class SubDataAnalysis implements Serializable {
 
 	@Column(name="sub_data_analysis_name")
 	private String subDataAnalysisName;
+
+	//bi-directional many-to-one association to ProviderHypothesi
+	@OneToMany(mappedBy="subDataAnalysis")
+	private List<ProviderHypothesis> providerHypothesis;
 
 	//bi-directional many-to-one association to DataAnalysi
 	@ManyToOne
@@ -55,12 +62,34 @@ public class SubDataAnalysis implements Serializable {
 		this.subDataAnalysisName = subDataAnalysisName;
 	}
 
+	public List<ProviderHypothesis> getProviderHypothesis() {
+		return this.providerHypothesis;
+	}
+
+	public void setProviderHypothesis(List<ProviderHypothesis> providerHypothesis) {
+		this.providerHypothesis = providerHypothesis;
+	}
+
+	public ProviderHypothesis addProviderHypothesis(ProviderHypothesis providerHypothesis) {
+		getProviderHypothesis().add(providerHypothesis);
+		providerHypothesis.setSubDataAnalysis(this);
+
+		return providerHypothesis;
+	}
+
+	public ProviderHypothesis removeProviderHypothesis(ProviderHypothesis providerHypothesis) {
+		getProviderHypothesis().remove(providerHypothesis);
+		providerHypothesis.setSubDataAnalysis(null);
+
+		return providerHypothesis;
+	}
+
 	public DataAnalysis getDataAnalysis() {
 		return this.dataAnalysis;
 	}
 
-	public void setDataAnalysis(DataAnalysis dataAnalysi) {
-		this.dataAnalysis = dataAnalysi;
+	public void setDataAnalysis(DataAnalysis dataAnalysis) {
+		this.dataAnalysis = dataAnalysis;
 	}
 
 }

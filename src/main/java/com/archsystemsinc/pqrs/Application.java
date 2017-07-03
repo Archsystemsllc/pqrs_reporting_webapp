422e2f72e3d56dc774aa197f60811c4172f4609f
grmahun <grmahun1@gmail.com>
2017-07-02 23:42:25 -0400

    jar package

--- src/main/java/com/archsystemsinc/pqrs/Application.java
+++ src/main/java/com/archsystemsinc/pqrs/Application.java
@@ -6,11 +6,11 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer{
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+public class Application {//extends SpringBootServletInitializer{
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Application.class);
+//    }
     
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
